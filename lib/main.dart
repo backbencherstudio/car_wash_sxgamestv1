2ea6bdcf8e8732c4946af05b48f8429a8668eb8d
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-04-19 16:40:32 +0600

    internet permission added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:car_wash/core/theme/theme.dart';
 import 'core/routes/route_config.dart';
 
