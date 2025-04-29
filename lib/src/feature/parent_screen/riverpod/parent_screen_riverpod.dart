368dbb65ddeee5cc1570fbf75528d92680a6406a
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-04-29 10:11:30 +0600

    Bottom Nav bar problem solved

--- lib/src/feature/parent_screen/riverpod/parent_screen_riverpod.dart
+++ lib/src/feature/parent_screen/riverpod/parent_screen_riverpod.dart
@@ -1,22 +1,56 @@
 import 'package:car_wash/src/feature/parent_screen/riverpod/parent_screen_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../home_screen/view/home_screen.dart';
+import '../dummy_screens/dummy1.dart';
+import '../dummy_screens/dummy2.dart';
 
-
-final parentsScreenProvider = StateNotifierProvider<ParentsScreenProvider, ParentScreenState>(
-        (ref) {
+final parentsScreenProvider =
+    StateNotifierProvider<ParentsScreenProvider, ParentScreenState>((ref) {
       return ParentsScreenProvider();
     });
 
-
-class ParentsScreenProvider extends StateNotifier<ParentScreenState>{
-
+class ParentsScreenProvider extends StateNotifier<ParentScreenState> {
   ParentsScreenProvider() : super(ParentScreenState());
 
+  PageController pageController = PageController(initialPage: 0);
 
-  void onSelectedIndex({required int index}) {
+  /// Safrid bhai should fix this
+  /// Please change ProfileScreen() to CompleteProfileScreen() and insert the real ProfileScreen class
+  /// to below list,
+  ///
+  /// List of Parent screen
+  List<Widget> pageList = [
+    HomeScreen(),
+    Love(),
+    FavouriteScreen(),
+    FavouriteScreen(),
+  ];
+
+  /// Call this method from bottom nav bar button
+  void onPageChange({required int index}) {
+    int indexDifference = (state.selectedIndex - index).abs();
     debugPrint("\nSelected Page index : $index\n");
     state = state.copyWith(selectedIndex: index);
+
+    /// If difference between pages is greater than 2 then jump to that page
+    /// or normally animate to that page
+    if (indexDifference > 2) {
+      pageController.jumpToPage(index);
+    } else {
+      pageController.animateToPage(
+        index,
+        duration: Duration(milliseconds: 200),
+        curve: Curves.linear,
+      );
+    }
+  }
+
+  /// call this method during drawer open or close event
+  void onDrawerOpenOrClose(bool value) {
+    state = state.copyWith(isDrawerOpened: value);
+    debugPrint(
+      "\nafter changing the endDrawerValue, value is : ${state.isDrawerOpened}\n",
+    );
   }
 }
-
--- lib/src/feature/parent_screen/riverpod/parent_screen_state.dart
+++ lib/src/feature/parent_screen/riverpod/parent_screen_state.dart
@@ -1,18 +1,12 @@
-import 'package:flutter/material.dart';
-import '../../home_screen/view/home_screen.dart';
-import '../../profile_screen/view/profile_screen.dart';
-import '../dummy_screens/dummy1.dart';
-import '../dummy_screens/dummy2.dart';
-
-class ParentScreenState{
-  final List<Widget> pageList;
+class ParentScreenState {
   final int selectedIndex;
-  ParentScreenState({
-    this.pageList = const [HomeScreen(),Love(), FavouriteScreen(), ProfileScreen()],
-    this.selectedIndex = 0,
-  });
-  ParentScreenState copyWith({List<Widget>? pageList, int? selectedIndex}){
-    return ParentScreenState(pageList: pageList ?? this.pageList, selectedIndex: selectedIndex ?? this.selectedIndex);
-  }
+  final bool isDrawerOpened;
 
+  ParentScreenState({this.selectedIndex = 0, this.isDrawerOpened = false});
+  ParentScreenState copyWith({int? selectedIndex, bool? isDrawerOpened}) {
+    return ParentScreenState(
+      selectedIndex: selectedIndex ?? this.selectedIndex,
+      isDrawerOpened: isDrawerOpened ?? this.isDrawerOpened,
+    );
+  }
 }
