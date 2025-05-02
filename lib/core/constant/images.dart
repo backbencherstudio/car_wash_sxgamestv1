ab7b8f13609453cbab2aaeee393f506e7be93724
df <fd>
2025-05-02 10:24:15 +0600

    pulll

--- lib/core/constant/images.dart
+++ lib/core/constant/images.dart
@@ -1,3 +1,6 @@
+import 'package:car_wash/src/feature/auth_screens/view/signup_screens/view/successfully_registered_screen.dart';
+import 'package:car_wash/src/feature/splash_screen/view/splash_screen.dart';
+
 class AppImages{
   /// all should be static
   static const screenBackgroundImage = "assets/images/background.png";
@@ -17,4 +20,22 @@ class AppImages{
 
   static const blogPhoto = "assets/images/home_screen/blog_picture.png";
   static const blogPhoto2 = "assets/images/home_screen/blog_picture2.png";
+
+  //forget pass
+   static const successfullyDone = "assets/images/forgot_password/success.png";
+   //service details
+   static const bently = "assets/images/serviceDetails/bently.png";
+   static const washCarBanner = "assets/images/serviceDetails/maskGroup.png";
+   //signup
+   static const registerPage = "assets/images/signup/register.png";
+   //timlineScreen
+   static const logo = "assets/images/Logo.png";
+   //splash screen
+   static const splashScreen = "assets/images/splash.png";
+   //profile screen
+   static const profile = "assets/images/profilepic.png";
+   //white background
+   static const whiteBackground= "assets/images/whitebgm.png";
+ 
+
 }
